b4738ec217c22bcc735e1e774400266e50a6bb75
Erin Skidds <dudethatserin@outlook.com>
2025-10-05 00:12:19 +0000

    latest

--- slashcommands/utility/wiki.js
+++ slashcommands/utility/wiki.js
@@ -1,11 +1,29 @@
+const { EmbedBuilder } = require('discord.js');
+const embedConfig = require('../../config/embed.json');
+
 module.exports = {
     name: 'wiki',
     description: 'Links to our wiki',
     usage: '/wiki',
     botSpamOnly: 1,
     execute(interaction) {
+        const embed = new EmbedBuilder()
+            .setColor(parseInt(embedConfig.light_green_color, 16))
+            .setTitle('📚 OneNote Wiki')
+            .setDescription('Check out our wiki for helpful information and guides!')
+            .addFields({
+                name: '🔗 Wiki Link',
+                value: '[Click here to visit our wiki](https://onenote-wiki.vercel.app)',
+                inline: false
+            })
+            .setTimestamp()
+            .setFooter({ 
+                text: embedConfig.footertext, 
+                iconURL: embedConfig.footericon 
+            });
+
         interaction.reply({ 
-            content: 'Check out our wiki for helpful information and guides: https://onenote-wiki.vercel.app',
+            embeds: [embed],
             flags: 64 
         });
     }
